b2bb4d74382c43a280d405a3c23284d3fcc269ed
Chu <chuh@isca191.mskcc.org>
2025-11-18 15:35:46 -0500

    updated ligandmpnn setu0p

--- workflows/LigandMPNN/set_up_LigandMPNN_workflow.sh
+++ workflows/LigandMPNN/set_up_LigandMPNN_workflow.sh
@@ -1,16 +1,18 @@
 ## Clone the LigandMPNN repo into the public directory
-git clone https://github.com/dauparas/LigandMPNN ../public
+CARPNN_DIR="/data1/lareauc/users/chuh/softwares/CARPNN"
+git clone https://github.com/dauparas/LigandMPNN ${CARPNN_DIR}/public/LigandMPNN
 
 ## Create the apporpriate ligandmpnn environment
 (
-    cd ../public/LigandMPNN
-    conda create -n ligandmpnn_env python=3.11
+    cd ${CARPNN_DIR}/public/LigandMPNN && \
+    conda create -n ligandmpnn_env python=3.11 && \
+    conda activate ligandmpnn_env && \
     pip3 install -r requirements.txt
 )
 
 ## download the af2 and mpnn model param weights
 (
-    cd ../public/LigandMPNN
+    cd ${CARPNN_DIR}/public/LigandMPNN && \
     bash ./get_model_params.sh "./model_params"
 )
 
